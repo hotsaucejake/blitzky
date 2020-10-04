bc927173b38a1b0285b91b82aefef8b06b932a45
hotsaucejake <get@hotsaucejake.com>
2020-10-04 10:09:28 -0400

    initial commit

---
+++ src/app/components/dropdowns/notification-dropdown/notification-dropdown.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+
+@Component({
+  selector: "app-notification-dropdown",
+  templateUrl: "./notification-dropdown.component.html",
+})
+export class NotificationDropdownComponent implements OnInit {
+  dropdownPopoverShow = false;
+  @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
+  
+  ngOnInit() {
+    
+  }
+  toggleDropdown(event) {
+    event.preventDefault();
+    if (this.dropdownPopoverShow) {
+      this.dropdownPopoverShow = false;
+      this.destroyPopper();
+    } else {
+      this.dropdownPopoverShow = true;
+      this.createPoppper();
+    }
+  }
+  destroyPopper() {
+    
+  }
+  createPoppper() {
+    
+  }
+}
---
+++ src/app/components/dropdowns/table-dropdown/table-dropdown.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+
+@Component({
+  selector: "app-table-dropdown",
+  templateUrl: "./table-dropdown.component.html",
+})
+export class TableDropdownComponent implements OnInit {
+  dropdownPopoverShow = false;
+  @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
+  
+  ngOnInit() {
+    
+  }
+  toggleDropdown(event) {
+    event.preventDefault();
+    if (this.dropdownPopoverShow) {
+      this.dropdownPopoverShow = false;
+      this.destroyPopper();
+    } else {
+      this.dropdownPopoverShow = true;
+      this.createPoppper();
+    }
+  }
+  destroyPopper() {
+    
+  }
+  createPoppper() {
+    
+  }
+}
